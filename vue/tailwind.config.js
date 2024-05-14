84b16b632964ba8794a9ac02653b2a7a6d24d373
bossthanawat <boss41680@gmail.com>
2024-05-14 20:25:44 +0700

    Add Vue project

---
+++ vue/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1000px",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
