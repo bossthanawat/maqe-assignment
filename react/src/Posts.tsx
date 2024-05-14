f904777b78ea5bcff035f77e12e25f29e5bd6c84
bossthanawat <boss41680@gmail.com>
2024-05-14 20:13:48 +0700

    Add posts

--- react/src/Posts.tsx
+++ react/src/Posts.tsx
@@ -1,5 +1,47 @@
-const Posts = () => {
-  return <></>
-}
+import { useQuery } from "@tanstack/react-query";
+import { getAuthors, getPosts } from "./service";
+import CardPosts from "./components/CardPosts";
 
-export default Posts
+const Posts = () => {
+  const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  const { data: posts } = useQuery({
+    queryKey: ["getPosts"],
+    queryFn: async () => {
+      const { data: posts } = await getPosts();
+      const { data: authors } = await getAuthors();
+      return posts.map((post) => {
+        const author = authors.find((author) => author.id === post.author_id);
+        return {
+          ...post,
+          author,
+        };
+      });
+    },
+  });
+  return (
+    <>
+      <div className="container mt-8">
+        <h1 className="text-2xl font-medium">MAQE Forum</h1>
+        <p className="mt-6">Your current timezone is: {timezone}</p>
+        <div className="flex flex-col gap-4 my-4">
+          {posts?.map((post, index) => (
+            <CardPosts
+              key={post.id}
+              title={post.title}
+              author={{
+                avatarUrl: post?.author?.avatar_url || "",
+                name: post?.author?.name || "",
+              }}
+              body={post.body}
+              image={post.image_url}
+              createdAt={post.created_at}
+              className={index % 2 === 0 ? "bg-white" : "bg-blue-100"}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Posts;
---
+++ react/src/components/CardPosts.tsx
@@ -0,0 +1,57 @@
+import dayjs from "dayjs";
+import { cn } from "../lib/utils";
+
+type CardPostsProps = {
+  title: string;
+  body: string;
+  image: string;
+  createdAt: string;
+  author: {
+    name: string;
+    avatarUrl: string;
+  };
+} & React.HTMLAttributes<HTMLDivElement>;
+
+const CardPosts = (props: CardPostsProps) => {
+  const { title, body, image, createdAt, author, ...another } = props;
+  const handleFormatDatePost = (datetime: string) => {
+    return dayjs(datetime).format("dddd, MMMM D, YYYY, HH:mm");
+  };
+  return (
+    <>
+      <div
+        {...another}
+        className={cn("shadow-md rounded-sm", another.className)}
+      >
+        <div className="px-4 py-2 border-b-[1px] border-gray-300">
+          <div className="flex gap-1 text-sm items-center">
+            <img
+              src={author?.avatarUrl}
+              alt="avatar"
+              className="w-5 h-5 rounded-full"
+            />
+            <span className="text-orange-600 font-medium">{author?.name}</span>
+            <span className="text-gray-500">
+              posted on {handleFormatDatePost(createdAt)}
+            </span>
+          </div>
+        </div>
+        <div className="p-4">
+          <div className="flex gap-4 items-start">
+            <img
+              src={image}
+              alt="post image"
+              className="max-w-[240px] object-contain"
+            />
+            <div>
+              <p className="text-lg font-medium">{title}</p>
+              <p className="mt-2 text-sm">{body}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CardPosts;
